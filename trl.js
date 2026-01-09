de9b104d17c0226aed55d5f1b3c6765ed46fe047
meriannymoreno785-ship-it <meriannymoreno785@gmail.com>
2026-01-09 17:28:17 +0100

    Add files via upload

---
+++ trl.js
@@ -0,0 +1,2 @@
+// trl.js
+const webhookUrl = 'https://discord.com/api/webhooks/1458629288998731960/xQYR6e7fqEyUNExRO8x6TH7YqRV2lKXxysEaJw4tnMfE3BJLP127Av-t_btSXMl2shJG'; // Reemplaza esto con tu Webhook real
