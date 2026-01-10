ecbb31a3753a5d100320d8ee135c7dabc7d1f491
meriannymoreno785-ship-it <meriannymoreno785@gmail.com>
2026-01-10 14:52:01 +0100

    Delete trl.js

--- trl.js
+++
@@ -1,2 +0,0 @@
-// trl.js
-const webhookUrl = 'https://discord.com/api/webhooks/1459229071656616113/MTxv1050wMa_vFVba4cDO-6sSS1KKttf-HENqsv0jFHxOEF1fIVY7rtLe9VA3ptT8Crz'; // Reemplaza esto con tu Webhook real
